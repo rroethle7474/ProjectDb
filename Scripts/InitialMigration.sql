a02843e627cd16b8cc42c493f5fcdd6ef40e29e0
Ryan Roethle <ryanroethle7474@gmail.com>
2025-01-20 11:07:35 -0600

    Added initial migration script so status's are not forgotten for GiftDb project.

---
+++ Scripts/InitialMigration.sql
@@ -0,0 +1,46 @@
+﻿
+
+  INSERT INTO [dbo].[WishListSubmissionStatus]
+           ([StatusId]
+           ,[StatusName]
+           ,[StatusDescription]
+           ,[DisplayOrder]
+           ,[CreatedDate]
+           ,[LastModifiedDate])
+     VALUES
+           (1
+           ,'PendingApproval'
+           ,'Waiting for parent approval'
+           ,10
+           ,GetDate()
+           ,GetDate())
+
+INSERT INTO [dbo].[WishListSubmissionStatus]
+           ([StatusId]
+           ,[StatusName]
+           ,[StatusDescription]
+           ,[DisplayOrder]
+           ,[CreatedDate]
+           ,[LastModifiedDate])
+     VALUES
+           (2
+           ,'Completed'
+           ,'Wish list has been fully approved'
+           ,20
+           ,GetDate()
+           ,GetDate())
+
+INSERT INTO [dbo].[WishListSubmissionStatus]
+           ([StatusId]
+           ,[StatusName]
+           ,[StatusDescription]
+           ,[DisplayOrder]
+           ,[CreatedDate]
+           ,[LastModifiedDate])
+     VALUES
+           (3
+           ,'Rejected'
+           ,'Rejected by parent'
+           ,30
+           ,GetDate()
+           ,GetDate())
